48e83822ffe9f8e2c74362c7efd8ccd4fbab6030
VieriuAniko <146959409+VieriuAniko@users.noreply.github.com>
2024-01-18 22:57:39 +0200

    Add files via upload

---
+++ Setul 2 - ex. 16 ##/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Setul_2___ex._16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Indicatie: O <secventa bitonica rotita> este o secventa bitonica sau una ca poate fi transformata intr-o secventa bitonica prin rotiri succesive
+            //(rotire = primul element devine ultimul).Se da o secventa de n numere. Se cere sa se determine daca este o secventa bitonica rotita. 
+
+            Console.WriteLine("Introduceti o secventa de numere: ");
+            string input = Console.ReadLine();
+
+            int secventa = int.Parse(input);
+
+            bool rezultat = EsteBitonicaRotita(secventa);
+
+            if (rezultat)
+                Console.WriteLine("Secventa este bitonica rotita");
+            else
+                Console.WriteLine("Secventa nu este bitonica rotita");
+
+            static bool EsteBitonicaRotita(int secventa)
+            {
+                int curent = 0, precedent = 0;
+
+                int n = secventa.ToString().Length;
+
+                int schimbari = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    curent = secventa % 10;
+                    precedent = (secventa / 10) % 10;
+                    int urmator = (secventa / 100) % 10;
+
+                    if ((curent < precedent && curent > urmator) || (curent > precedent && curent < urmator))
+                        schimbari++;
+
+                    secventa /= 10;
+                }
+
+                if (schimbari > 2)
+                    return false;
+
+                if (schimbari == 2)
+                {
+                    int prima = secventa % 10;
+                    int ultima = curent;
+
+                    int aDoua = (secventa / 10) % 10;
+                    int penultima = precedent;
+
+                    if ((prima < ultima && aDoua < prima && penultima < ultima) || (prima > ultima && aDoua > prima && penultima > ultima))
+                        return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
---
+++ Setul 2 - ex. 17 ##/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Setul_2___ex._17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Indicatie: Se da o secventa de 0 si 1, unde 0 inseamna paranteza deschisa si 1 inseamna paranteza inchisa.
+            //Determinati daca secventa reprezinta o secventa de paranteze corecta si,  daca este, determinati nivelul maxim de incuibare a parantezelor.
+            //De exemplu 0 1 0 0 1 0 1 1 este corecta si are nivelul maxim de incuibare 2 pe cand 0 0 1 1 1 0 este incorecta.
+
+            string s = "01001011"; //secventa citita
+
+            Stack<int> stack = new Stack<int>();
+
+            int level = 0, maxLevel = 0;
+            bool isCorrect = true;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '0')
+                {
+                    stack.Push(i);
+                    level++;
+
+                    if (level > maxLevel)
+                        maxLevel = level;
+                }
+                else if (s[i] == '1')
+                {
+                    if (stack.Count == 0)
+                    {
+                        isCorrect = false;
+                        break;
+                    }
+                    else
+                    {
+                        int j = stack.Pop();
+                        if (s[j] != '0')
+                        {
+                            isCorrect = false;
+                            break;
+                        }
+                        else
+                            level--;
+                    }
+                }
+            }
+
+            if (stack.Count > 0)
+                isCorrect = false;
+
+            if (isCorrect)
+                Console.WriteLine($"Secventa este corecta si are nivelul maxim de incuibare {maxLevel}");
+            else
+                Console.WriteLine("Secventa este incorecta");
+        }
+    }
+}
